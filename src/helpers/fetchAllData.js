8b5275363991936314e721a97a29997e0e62ecba
Dan Schuman <danschuman@gmail.com>
2015-11-13 16:54:41 -0800

    Guard against 'undefined'
    Not sure why I didn't experience this before with the old router. I have a router like this, with empty Route wrapper: 
    
    ```
    <Route>
     {_DEV_ && <Route ...devRoute/>}
    <Route to="/" component={App}/>
    </Route>
    ```

--- src/helpers/fetchAllData.js
+++ src/helpers/fetchAllData.js
@@ -2,6 +2,7 @@
 function fetchAllData(components, getState, dispatch, location, params, deferred) {
   const methodName = deferred ? 'fetchDataDeferred' : 'fetchData';
   return components
+    .filter((component) => !!component) // Weed out 'undefined' routes
     .filter((component) => component[methodName]) // only look at ones with a static fetchData()
     .map((component) => component[methodName])    // pull out fetch data methods
     .map(fetchData =>
